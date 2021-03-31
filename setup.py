29241d5047d6e0cc14c4e87d06816ad703d1d281
rahuliitb <rahuliitb16@gmail.com>
2021-03-31 18:18:22 +0530

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='obs_test',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='git@github.com:rahuliitb16/test_pakage.git',
+    author='Rahul Kumar',
+    author_email='rahuliitb16@gmail.com',
+    license='unlicense',
+    packages=['obs_test'],
+    zip_safe=False
+)
