d135c90a750d6215f117b074ffcfd59312cddc54
rahuliitb <rahuliitb16@gmail.com>
2021-04-05 00:54:04 +0530

    change the obst_test to test_obs in setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='obs_test',
+    name='test_obs',
     version='0.0.1',
     description='My private package from private github repo',
-    url='git@github.com:rahuliitb16/test_pakage.git',
+    url='git@github:git@github.com:rahuliitb16/test_pakage.git',
     author='Rahul Kumar',
     author_email='rahuliitb16@gmail.com',
     license='unlicense',
-    packages=['obs_test'],
+    packages=['test_obs'],
     zip_safe=False
 )
